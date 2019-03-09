ede3c7a4065bab9d78857380d2d96f6918e42e8e
bbvch13531 <bbvch13531@gmail.com>
2019-03-09 19:35:33 +0900

    FEAT: Add comments
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- Algorithms/MergeSort.swift
+++ Algorithms/MergeSort.swift
@@ -8,9 +8,12 @@ public func MergeSort<T: Comparable>(_ array: [T]) -> [T] {
     guard array.count > 1 else { return array }
 
     let mid = array.count / 2
+
+    // Split into two parts
     let left = MergeSort(Array(array[0..<mid]))
     let right = MergeSort(Array(array[mid..<array.count]))
 
+    // Then merge sorted parts
     return merge(leftPile: left, rightPile: right)
 }
 
@@ -18,19 +21,24 @@ private func merge<T: Comparable>(leftPile: [T], rightPile: [T]) -> [T] {
     var left = 0
     var right = 0
     var orderedPile: [T] = []
+
+    // When array size is too small, resize it.
     if orderedPile.capacity < leftPile.count + rightPile.count {
         orderedPile.reserveCapacity(leftPile.count + rightPile.count)
     }
 
     while true {
         guard left < leftPile.endIndex else {
+            // End of leftPile
             orderedPile.append(contentsOf: rightPile[right..<rightPile.endIndex])
             break
         }
         guard right < rightPile.endIndex else {
+            // End of rightPile
             orderedPile.append(contentsOf: leftPile[left..<leftPile.endIndex])
             break
         }
+        // Append smaller one.
         if(leftPile[left] < rightPile[right]) {
             orderedPile.append(leftPile[left])
             left = left + 1
