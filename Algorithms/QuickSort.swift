782886cc36ec7e4b6c55b1b3b0a9e6bd47ff4659
bbvch13531 <bbvch13531@gmail.com>
2019-03-09 17:16:44 +0900

    FEAT: Finish quicksort
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- Algorithms/QuickSort.swift
+++ Algorithms/QuickSort.swift
@@ -1,17 +1,36 @@
 // https://github.com/bbvch13531/SSU-cse-algorithm/blob/master/quickSort/main.cpp
 public func QuickSort<T: Comparable>(_ array: [T], lo: Int, hi: Int) -> [T] {
+    var arr = array
+    var pi: Int = 0
+    var smallerArr = [T](), biggerArr =  [T]()
+    if lo < hi {
+        pi = partition(&arr, lo: lo, hi: hi);
 
+        smallerArr = QuickSort(arr, lo: lo, hi: pi - 1)
+        biggerArr = QuickSort(arr, lo: pi + 1, hi: hi)
+
+        smallerArr.append(arr[pi])
+        smallerArr.append(contentsOf: biggerArr)
+        
+        return smallerArr
+    } else if lo == hi {    
+        return [arr[lo]]
+    }    
+    return [T]()
 }
 
-func partition<T: Comparable>(_ array: [T], lo: Int, hi: Int) -> Int {
-    var arr = array
+func partition<T: Comparable>(_ array: inout [T], lo: Int, hi: Int) -> Int {
     var pivot = array[hi]
-    var pivotIndex = hi
-    var tmp
-
-    i in lo..<hi {
-        if(arr[i] < pivot) {
-            pi
+    
+    var i = lo
+    for j in lo..<hi {
+        if(array[j] <= pivot) {
+            // (array[j], array[i]) = (array[i], array[j])
+            array.swapAt(i,j)
+            i += 1
         }
     }
+    // (array[i], array[hi]) = (array[hi], array[i])
+    array.swapAt(i,hi)    
+    return i
 }
